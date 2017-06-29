427677f816514fe9249186da803fe09e751141b2
Dario Motto <dario_aratto@hotmail.com>
2017-06-29 03:00:51 -0600

    Cascaron

--- app/Http/Controllers/MTitularesGenero.php
+++ app/Http/Controllers/MTitularesGenero.php
@@ -13,6 +13,7 @@ use App\Bono;
 use App\Beneficiario;
 use App\Titular;
 use DateTime;
+use DB;
 
 class MTitularesGenero extends Controller
 {
@@ -59,26 +60,40 @@ class MTitularesGenero extends Controller
      */
     public function store(Request $request)
     {
+        //dd($request->all());
+        $canton = $request->canton;
         $hombre = 0;
         $mujer = 0;
-
-        $beneficiario = Beneficiario::where('Canton_id', $request->canton)->get();
-        //dd($beneficiario);
-        foreach($beneficiario as $bene){
-            $bene->titular = Titular::where('id', $bene->Titular_id);
-        }
-        //dd($beneficiario);
-        foreach($beneficiario as $bene){
-           //dd($bene);
-            if( strcmp ( $bene->genero , 'm' ) == 0 || strcmp ( $bene->genero , 'M' ) == 0 ){
-                $hombre = $hombre + 1;
+        $edad_inicio = $request->edad_inicio;
+        $edad_fin = $request->edad_fin;
+        
+        $titular = DB::table('titular')
+            ->join('beneficiario', 'beneficiario.Titular_id', '=', 'titular.id')
+            ->select('titular.genero','titular.fechaNacimiento')
+            ->where('beneficiario.Canton_id', $canton)
+            ->get();
+        
+        foreach($titular as $tit){           
+            if( strcmp ( $tit->genero , 'm' ) == 0 || strcmp ( $tit->genero , 'M' ) == 0 ){
+                $edad =  $this->calcular_edad($bene->fechaNacimiento);
+                //Restriccion de Edad para Hombre
+                if(  $edad >= $edad_inicio && $edad <= $edad_fin )
+                    $hombre = $hombre + 1;
             }else{
-                $mujer = $mujer +1;
+                $edad =  $this->calcular_edad($bene->fechaNacimiento);
+                //Restriccion de Edad para Hombre
+                if(  $edad >= $edad_inicio && $edad <= $edad_fin )
+                    $mujer = $mujer +1;
             }
         }
         
-        return view('tit_genero.resultado');
-        //dd($request->all());
+        return view('tit_genero.resultado')->with([
+            'hombre' => $hombre,
+            'mujer' => $mujer,
+            'canton' => $canton,
+            'edad_inicio' => $edad_inicio,
+            'edad_fin' => $edad_fin,
+        ]);
     }
 
     /**
@@ -124,5 +139,12 @@ class MTitularesGenero extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function calcular_edad($fecha){
+        $dias = explode("-", $fecha, 3);
+        $dias = mktime(0,0,0,$dias[2],$dias[1],$dias[0]);
+        $edad = (int)((time()-$dias)/31556926 );
+        return $edad;
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -51,14 +51,70 @@ Route::get('/admin', 'HomeController@index');
           'uses' => 'MTitularesGenero@store', 
           'as'   => 'titulares_genero_cargar'
         ]);
-        Route::post('titulares_genero_resultado', [
-          'uses' => 'MTitularesGenero@resultado', 
-          'as'   => 'titulares_genero_resultado'
-        ]);
+        Route::get('/tit_genero_resultado', function () {
+          return view('tit_genero.resultado');
+        });
+        
         /*
         * Fin Rutas para Titulares por Genero y Edad
         */
+
+        /*
+        * Inicio Rutas para Titulares Adulto
+        */
+        Route::get('titulares_adulto',[
+          'uses' => 'MTitularesAdulto@index',
+          'as' => 'tit_adulto'
+        ]);
+        Route::post('titulares_adulto_cargar', [
+          'uses' => 'MTitularesAdulto@store', 
+          'as'   => 'titulares_adulto_cargar'
+        ]);
+        Route::get('/tit_adulto_resultado', function () {
+          return view('tit_adulto.resultado');
+        });
         
+        /*
+        * Fin Rutas para Titulares Adulto
+        */
+        
+        /*
+        * Inicio Rutas para Titulares Niños y Embarazada
+        */
+        Route::get('titulares_ne',[
+          'uses' => 'MTitularesNE@index',
+          'as' => 'tit_ne'
+        ]);
+        Route::post('titulares_ne_cargar', [
+          'uses' => 'MTitularesNE@store', 
+          'as'   => 'titulares_ne_cargar'
+        ]);
+        Route::get('/tit_ne_resultado', function () {
+          return view('tit_ne.resultado');
+        });
+        
+        /*
+        * Fin Rutas para Titulares Niños y Embarazada
+        */
+
+        /*
+        * Inicio Rutas para Fallecido Adulto
+        */
+        Route::get('fallecido_adulto',[
+          'uses' => 'MFallecidoAdulto@index',
+          'as' => 'fallecido_adulto'
+        ]);
+        Route::post('fallecido_adulto_cargar', [
+          'uses' => 'MFallecidoAdulto@store', 
+          'as'   => 'fallecido_adulto_cargar'
+        ]);
+        Route::get('/fallecido_adulto_resultado', function () {
+          return view('fallecido_adulto.resultado');
+        });
+        
+        /*
+        * Fin Rutas para Fallecido Adulto
+        */
 /*
 *
 * FIN RUTAS MOTTO
