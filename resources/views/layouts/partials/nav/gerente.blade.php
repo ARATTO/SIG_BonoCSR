9eaea4325f5b3a88137d2ad10068ffa2245875af
sjmendoza <sharylmendoza@gmail.com>
2017-06-30 00:40:12 -0600

    Mis reportes validados

--- resources/views/layouts/partials/nav/gerente.blade.php
+++ resources/views/layouts/partials/nav/gerente.blade.php
@@ -8,15 +8,11 @@
                     <a href="#"><i class='fa fa-link'></i> <span>{{ trans('REPORTES GERENCIALES') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
                     <ul class="treeview-menu">
                         <li><a href="{{route('seleccionarDatos')}}">{{ trans('Inversion en Salud y adulto mayor') }}</a></li>
-<<<<<<< HEAD
+
                         <li><a href="{{route('tit_genero')}}">Titulares divididos por Genero.</a></li>
                         <li><a href="#Motto">Titulares de niños y embarazadas.</a></li>
                         <li><a href="#Motto">Titulares de adulto mayor.</a></li>
-=======
-                        <li><a href="{{route('tit_genero')}}">M-Titulares divididos por Genero.</a></li>
-                        <li><a href="{{route('tit_ne')}}">M-Titulares de niños y embarazadas.</a></li>
-                        <li><a href="#Motto">M-Titulares de adulto mayor.</a></li>
->>>>>>> 123cac5e3d57fe95599e9b376f0cb140243ed200
+
                         <li><a href="#">Inversion total, eduacion y salud.</a></li>
                         <li><a href="#">Dinero invertido en promotores</a></li>
                     </ul>
