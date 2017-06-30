4dca71f3eb770fb76b52b17822761186e8674e95
MOTTO-Blame <dario_aratto@hotmail.com>
2017-06-30 10:31:27 -0600

    mejora menus

--- resources/views/layouts/partials/nav/gerente.blade.php
+++ resources/views/layouts/partials/nav/gerente.blade.php
@@ -8,9 +8,9 @@
                     <a href="#"><i class='fa fa-link'></i> <span>{{ trans('REPORTES GERENCIALES') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
                     <ul class="treeview-menu">
                         <li><a href="{{route('seleccionarDatos')}}">{{ trans('Inversion en Salud y adulto mayor') }}</a></li>
-                        <li><a href="{{route('tit_genero')}}">M-Titulares divididos por Genero.</a></li>
-                        <li><a href="{{route('tit_ne')}}">M-Titulares de niños y embarazadas.</a></li>
-                        <li><a href="{{route('fallecido_adulto')}}">M-Titulares de adulto mayor.</a></li>
+                        <li><a href="{{route('tit_genero')}}">Titulares divididos por Genero.</a></li>
+                        <li><a href="{{route('tit_ne')}}">Titulares de niños y embarazadas.</a></li>
+                        <li><a href="{{route('fallecido_adulto')}}">Titulares de adulto mayor.</a></li>
                         <li><a href="#">Inversion total, eduacion y salud.</a></li>
                         <li><a href="#">Dinero invertido en promotores</a></li>
                     </ul>
